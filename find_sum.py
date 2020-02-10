8c09f4801cf6fbb983dbeb5dfba110119d0c40a2
mustafa-1105 <mustafabohari11@gmail.com>
2020-02-11 02:21:12 +0530

    Find index of 2 intergers whose sum is equal to the given number

---
+++ find_sum.py
@@ -0,0 +1,16 @@
+"""
+Find index of 2 intergers whose sum is equal to the given number
+Problem link: https://leetcode.com/problems/two-sum/
+"""
+
+nums = [2, 7, 11, 15]
+target = 9
+
+a = dict()
+index = 0
+for i in nums:
+    diff = target - i
+    if (diff in a):
+        print([index, a[diff]])
+    a[i] = index
+    index += 1
